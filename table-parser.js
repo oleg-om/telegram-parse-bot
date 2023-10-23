202f71cdab7d9814c1f381183701a9c6cd7320dd
Oleg Omelchenko <oleg.omelc93@gmail.com>
2023-10-23 20:50:44 +0300

    google sheet parser | logger

--- table-parser.js
+++ table-parser.js
@@ -1,10 +1,21 @@
 import PublicGoogleSheetsParser from "public-google-sheets-parser";
 import { config } from "dotenv";
+import winston, { createLogger } from "winston";
 
 config();
 
 var { SHEET_ID, TABLE_ID, AVAILABLE_CHAT_IDS } = process.env;
 const iDS_ARRAY = AVAILABLE_CHAT_IDS.split(",");
+
+const logger = createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/info.log", level: "info" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
 
 const stickers = {
   newRow:
@@ -32,13 +43,36 @@ const parseTable = async (bot) => {
     if (rowsLength) {
       if (rowsLength === rows) {
         console.log("rows are same...");
+        logger.log({
+          level: "error",
+          rows: "rows are the same",
+          table: items?.slice(-10),
+          message: "not sent",
+          length: items?.length,
+          date: new Date(),
+        });
+
         return null;
       } else {
         rows = rowsLength;
         await sendMessage(bot);
+
+        logger.log({
+          level: "info",
+          rows: "new rows",
+          table: items?.slice(-10),
+          message: "sent",
+          length: items?.length,
+          date: new Date(),
+        });
       }
     }
   });
 };
 
+// TODO mock func
+// parseTable({
+//   sendSticker: () => {},
+//   sendMessage: () => {},
+// });
 export default parseTable;
